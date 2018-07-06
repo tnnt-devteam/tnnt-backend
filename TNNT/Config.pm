4245a3e51e03f45e6ee82317b084ebd84cbfd778
Borek Lupomesky <borek@lupomesky.cz>
2018-07-06 13:21:38 +0200

    Add basic JSON config loading

---
+++ TNNT/Config.pm
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+
+#=============================================================================
+# Loading and access to configuration
+#=============================================================================
+
+
+
+package TNNT::Config;
+
+use Moo;
+use JSON;
+
+
+
+#=============================================================================
+#=== ATTRIBUTES ==============================================================
+#=============================================================================
+
+has config_file => (
+  is => 'ro',
+  required => 1,
+);
+
+has config => (
+  is => 'lazy',
+  builder => '_load_config',
+);
+
+
+#=============================================================================
+# Config loading
+#=============================================================================
+
+sub _load_config
+{
+  my ($self) = @_;
+
+ if(-r $self->config_file()) {
+   local $/;
+   my $js = new JSON->relaxed(1);
+   open(my $fh, '<', $self->config_file());
+   my $def_json = <$fh>;
+   my $cfg = $js->decode($def_json) or die 'Cannot parse the configuration';
+   return $cfg;
+ } else {
+   die 'Cannot read config file ' . $self->config_file();
+ }
+}
+
+
+1;
