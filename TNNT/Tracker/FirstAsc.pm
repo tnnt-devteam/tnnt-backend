7b87b3dbc2f22a7d9696269dd28f6d0a2f5d5b85
Borek Lupomesky <borek@lupomesky.cz>
2018-07-17 15:31:07 +0200

    Add tracker for the "First Ascension" trophy

---
+++ TNNT/Tracker/FirstAsc.pm
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+
+#=============================================================================
+# Tracker for the "First Ascension" trophy
+#=============================================================================
+
+package TNNT::Tracker::FirstAsc;
+
+use Moo;
+use TNNT::ScoringEntry;
+
+
+
+#=============================================================================
+#=== ATTRIBUTES ==============================================================
+#=============================================================================
+
+has name => (
+  is => 'ro',
+  default => 'firstasc',
+);
+
+has player => (
+  is => 'rwp',
+);
+
+has game => (
+  is => 'rwp',
+);
+
+
+
+#=============================================================================
+#=== METHODS =================================================================
+#=============================================================================
+
+sub process_game
+{
+  my (
+    $self,
+    $game,
+    $player
+  ) = @_;
+
+  #--- if the game is ascended and its endtime is lower than the current's
+  #--- first ascension's endtime, or there is not current ascension, record it
+
+  if(
+    $game->is_ascended()
+    && (
+      !$self->game()
+      || $game->endtime() < $self->game()->endtime()
+    )
+  ) {
+
+    #--- remove scoring entry from previous holder (if any)
+
+    if($self->player()) {
+      $self->player()->remove_score('firstasc');
+    }
+
+    #--- set player and game
+
+    $self->_set_game($game);
+    $self->_set_player($player);
+
+    #--- add scoring entry to new holder
+
+    $player->add_score(new TNNT::ScoringEntry(
+      trophy => 'firstasc',
+      games => [ $game ],
+    ));
+  }
+}
+
+
+
+#=============================================================================
+
+1;
