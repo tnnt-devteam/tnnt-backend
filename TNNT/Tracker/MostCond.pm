611a5e911b06e6fcd291245d385b4dde835e0365
Borek Lupomesky <borek@lupomesky.cz>
2018-09-10 21:35:46 +0200

    Modify MostCond tracker to track Clan trophy as well

--- TNNT/Tracker/MostCond.pm
+++ TNNT/Tracker/MostCond.pm
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
 
 #=============================================================================
-# Tracker for the "Most Conducts in one Ascensions" trophy
+# Tracker for the "Most Conducts in one Ascensions" trophy (both individual
+# players and clans).
 #=============================================================================
 
 package TNNT::Tracker::MostCond;
@@ -24,6 +25,10 @@ has player => (
   is => 'rwp',
 );
 
+has clan => (
+  is => 'rwp',
+);
+
 has game => (
   is => 'rwp',
 );
@@ -43,6 +48,8 @@ sub add_game
 {
   my ($self, $game) = @_;
   my $player = $game->player();
+  my $clans = TNNT::ClanList->instance();
+  my $clan = $clans->find_clan($player->name());
 
   #--- count only ascending games
 
@@ -54,23 +61,41 @@ sub add_game
 
   #--- remove scoring entry from previous holder (if any)
 
-  if($self->player()) {
+    if($self->player()) {
       $self->player()->remove_score($self->name());
     }
+    if($self->game()) {
+      $self->game()->remove_score($self->name());
+    }
+    if($self->clan()) {
+      $self->clan()->remove_score('clan-' . $self->name());
+    }
 
-  #--- set player and game
+  #--- set player, game and clan
 
     $self->_set_game($game);
     $self->_set_player($game->player());
+    $self->_set_clan($clan) if $clan;
 
   #--- add scoring entry to new holder
 
-    $game->player()->add_score(new TNNT::ScoringEntry(
+    my $se_player = new TNNT::ScoringEntry(
       trophy => $self->name(),
       games  => [ $game ],
       data   => { nconds => scalar($game->conducts()) },
       when   => $game->endtime(),
-    ));
+    );
+
+    my $se_clan = new TNNT::ScoringEntry(
+      trophy => 'clan-' . $self->name(),
+      games  => [ $game ],
+      data   => { nconds => scalar($game->conducts()) },
+      when   => $game->endtime(),
+    );
+
+    $game->player()->add_score($se_player);
+    $game->add_score($se_player);
+    $clan->add_score($se_clan) if $clan;
 
   #--- store new max value
 
