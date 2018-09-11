f44f99c3eb12dab6be29189e553dbefb1f21a60e
Borek Lupomesky <borek@lupomesky.cz>
2018-09-11 10:29:55 +0200

    Add clan scoring to FirstAsc (first ascension) tracker

--- TNNT/Tracker/FirstAsc.pm
+++ TNNT/Tracker/FirstAsc.pm
@@ -1,7 +1,8 @@
 #!/usr/bin/env perl
 
 #=============================================================================
-# Tracker for the "First Ascension" trophy
+# Tracker for the "First Ascension" trophy (both individual players and
+# clans).
 #=============================================================================
 
 package TNNT::Tracker::FirstAsc;
@@ -24,6 +25,10 @@ has player => (
   is => 'rwp',
 );
 
+has clan => (
+  is => 'rwp',
+);
+
 has game => (
   is => 'rwp',
 );
@@ -36,10 +41,10 @@ has game => (
 
 sub add_game
 {
-  my (
-    $self,
-    $game,
-  ) = @_;
+  my ($self, $game) = @_;
+  my $player = $game->player();
+  my $clans = TNNT::ClanList->instance();
+  my $clan = $clans->find_clan($player->name());
 
   #--- if the game is ascended and its endtime is lower than the current's
   #--- first ascension's endtime, or there is not current ascension, record it
@@ -57,19 +62,36 @@ sub add_game
     if($self->player()) {
       $self->player()->remove_score('firstasc');
     }
+    if($self->game()) {
+      $self->game()->remove_score($self->name());
+    }
+    if($self->clan()) {
+      $self->clan()->remove_score('clan-' . $self->name());
+    }
 
-    #--- set player and game
+    #--- set player, game and clan
 
     $self->_set_game($game);
     $self->_set_player($game->player());
+    $self->_set_clan($clan) if $clan;
 
     #--- add scoring entry to new holder
 
-    $game->player()->add_score(new TNNT::ScoringEntry(
+    my $se_player = new TNNT::ScoringEntry(
       trophy => 'firstasc',
       games => [ $game ],
       when => $game->endtime(),
-    ));
+    );
+
+    my $se_clan = new TNNT::ScoringEntry(
+      trophy => 'clan-firstasc',
+      games => [ $game ],
+      when => $game->endtime(),
+    );
+
+    $game->player()->add_score($se_player);
+    $game->add_score($se_player);
+    $clan->add_score($se_clan) if $clan;
   }
 
   return $self;
