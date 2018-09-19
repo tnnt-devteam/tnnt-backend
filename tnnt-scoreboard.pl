cd2c2499f92bfa06c3b59382bc034667df1bd655
Borek Lupomesky <borek@lupomesky.cz>
2018-09-19 14:26:11 +0200

    Create an initial version of the scoreboard script
    It reads the xlogfiles, compiles the scoreboard and exits. At the moment
    it doesn't produce any output.

---
+++ tnnt-scoreboard.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+
+#=============================================================================
+# THE NOVEMBER NETHACK TOURNAMENT
+# """""""""""""""""""""""""""""""
+# Scoreboard generator.
+#=============================================================================
+
+use v5.10;
+
+use Try::Tiny;
+use Moo;
+use TNNT::Cmdline;
+use TNNT::Config;
+use TNNT::Source;
+use TNNT::ClanList;
+use TNNT::Score;
+
+$| = 1;
+
+#--- process command-line arguments
+
+my $cmd;
+
+try {
+  $cmd = TNNT::Cmdline->instance();
+} catch {
+  print STDERR "Invalid command-line arguments\n";
+  exit(1);
+};
+
+#--- load configuration file
+
+my $cfg = TNNT::Config->instance(config_file => 'config.json');
+$cfg->config();
+
+#--- load clan list
+
+my $clans = TNNT::ClanList->instance();
+
+#--- initialize sources
+
+my @sources;
+
+$cfg->iter_sources(sub {
+  push(@sources, TNNT::Source->new(@_));
+});
+
+#--- perform reading of the xlogfiles
+
+my $score = TNNT::Score->new();
+
+for my $src (@sources) {
+  $src->read(sub { $score->add_game($_[0]) });
+}
+
+#--- compile the scoreboard
+
+$score->process();
+
