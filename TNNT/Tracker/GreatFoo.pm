b089275b479cb4ed0afbb3f66ac496f023f792df
Borek Lupomesky <borek@lupomesky.cz>
2018-10-02 11:35:02 +0200

    Adapt the GreatFoo tracker to a new clan trophy definition
    Great/Less Race/Role clan trophies can now achieved collaboratively by
    clan members (before they needed to be achieved in their entirerty by one
    clan member).

--- TNNT/Tracker/GreatFoo.pm
+++ TNNT/Tracker/GreatFoo.pm
@@ -7,6 +7,7 @@
 
 package TNNT::Tracker::GreatFoo;
 
+use Carp;
 use Moo;
 use TNNT::ScoringEntry;
 use TNNT::Tracker::MultiSet;
@@ -39,19 +40,47 @@ has _clan_track => (
 #=============================================================================
 
 #-----------------------------------------------------------------------------
-# Return player tracking entry. If it doesn't exist yet, new one is created
-# and returned.
+# Return a ref to player or clan tracking structure.
 #-----------------------------------------------------------------------------
 
-sub _ptrack
+sub _track
 {
-  my ($self, $player) = @_;
+  my ($self, $subj_type) = @_;
 
-  if(!exists $self->_player_track()->{$player->name()}) {
-    $self->_player_track()->{$player->name()} = {};
+  if($subj_type eq 'player') {
+    return $self->_player_track();
+  } elsif($subj_type eq 'clan') {
+    return $self->_clan_track();
   }
 
-  return $self->_player_track()->{$player->name()};
+  croak "Invalid argument to GreatFoo->_track($subj_type)";
+}
+
+
+#-----------------------------------------------------------------------------
+# Return player/clan tracking entry. If it doesn't exist yet, new one is
+# created and returned. The argument must be an instance of Player or Clan.
+#-----------------------------------------------------------------------------
+
+sub _track_data
+{
+  my ($self, $subj) = @_;
+
+  if($subj->isa('TNNT::Player')) {
+    if(!exists $self->_player_track()->{$subj->name()}) {
+      return $self->_player_track()->{$subj->name()} = {};
+    } else {
+      return $self->_player_track()->{$subj->name()};
+    }
+  } elsif($subj->isa('TNNT::Clan')) {
+    if(!exists $self->_clan_track()->{$subj->n()}) {
+      return $self->_clan_track()->{$subj->n()} = {};
+    } else {
+      return $self->_clan_track()->{$subj->n()};
+    }
+  } else {
+    croak 'Invalid argument to GreatFoo->track_data(), must be Player or Clan';
+  }
 }
 
 
@@ -69,97 +98,118 @@ sub add_game
   my $clan = $player->clan();
   my $cfg = TNNT::Config->instance()->config();
 
-  #--- initialize player tracking
+  #--- initialize player/clan tracking
 
-  my $ptrk = $self->_ptrack($player);
+  # The following code with its nested four levels deep for loops sets up
+  # a MultiSet instance for each player/clan and trophy. It also sets up
+  # a callback that is invoked when the MultiSet tracker achieves the
+  # 'fullfilled' state. This callback creates the scoring entries and adds
+  # player/clan to list of trophy holders.
 
-  if(!%$ptrk) {
-    for my $the ('great', 'lesser' ) {
-      for my $foo ('race', 'role') {
-        my $the_foo = "$the$foo";
+  for my $subj ($player, $clan) {
 
-        # iterate over all combinatins of race-align or role-align defined in the
-        # configuration. $rr stands for 'race or role'; note that great and lesser
-        # foo have the same allowed combinations, that's why only 'greatfoo' are
-        # defined in configuration
+    # this weeds out games that are non-clan games (ie. player is not a member
+    # of a clan
 
-        for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
+    next if !$subj;
 
-        # create MultiSet tracking instance, with an embedded callback to create
-        # scoring entry when the MultiSet flips to achieve state; note, that we
-        # need to set a 'loose' mode of the MultiSet instance so that irrelevant
-        # input is ignored instead of failing
+    # get the data structure that will be used to hold tracking data
 
-          $ptrk->{$the_foo}{$rr} = TNNT::Tracker::MultiSet->new_sets(
-            $rr => $cfg->{'nethack'}{"great$foo"}{$rr},
-            sub {
-              # score player
-              $player->add_score(TNNT::ScoringEntry->new(
-                trophy => $the_foo . ':' . lc($rr),
-                game => [ $_[0] ],
-                when => $_[0]->endtime()
-              ));
-              # score player's clan
-              if(
-                $clan
-                &&
-                !exists $self->_clan_track()->{$clan->n()}{$the_foo}{$rr}
-              ) {
-                $clan->add_score(TNNT::ScoringEntry->new(
-                  trophy => 'clan-' . $the_foo . ':' . lc($rr),
-                  game => [ $_[0] ],
-                  when => $_[0]->endtime(),
-                  data => { player_name => $player->name() }
-                ));
-                $self->_clan_track()->{$clan->n()}{$the_foo}{$rr} = $game;
-              }
+    my $trk = $self->_track_data($subj);
+
+    if(!%$trk) {
+      for my $the ('great', 'lesser' ) {
+        for my $foo ('race', 'role') {
+          my $the_foo = "$the$foo";
+
+          # iterate over all combinatins of race-align or role-align defined in
+          # the configuration. $rr stands for 'race or role'; note that great
+          # and lesser foo have the same allowed combinations, that's why only
+          # 'greatfoo' are defined in configuration
+
+          for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
+
+          # get trophy name (clans get the "clan-" prefix)
+
+            my $trophy_name = $the_foo . ':' . lc($rr);
+            if($subj->isa('TNNT::Clan')) {
+              $trophy_name = 'clan-' . $trophy_name;
             }
-          );
-          $ptrk->{$the_foo}{$rr}->mode('loose');
+
+          # create MultiSet tracking instance, with an embedded callback to
+          # create scoring entry when the MultiSet flips to achieve state;
+          # note, that we need to set 'loose' mode of the MultiSet instance so
+          # that irrelevant input is ignored instead of failing
+
+            $trk->{$the_foo}{$rr} = TNNT::Tracker::MultiSet->new_sets(
+              $rr => $cfg->{'nethack'}{"great$foo"}{$rr},
+              sub {
+                # score player
+                $subj->add_score(TNNT::ScoringEntry->new(
+                  trophy => $trophy_name,
+                  game => [ $_[0] ],
+                  when => $_[0]->endtime()
+                ));
+              }
+            );
+            $trk->{$the_foo}{$rr}->mode('loose');
+          }
         }
       }
     }
   }
 
-  #--- track players
+  #--- track players/clans
 
-  # the same iteration as for intialization, but instead of creating MultiSet
-  # instances we invoke their 'track' method; when the MultiSets go into
-  # attained state, they automatically invoke callbacks we defined above to
-  # create scoring entries
+  # the same iteration as for intialization, but here we invoke their 'track'
+  # method; this is all we need to do, because the callbacks will we defined
+  # above will do all the work when the MultiSet trackers trigger
 
-  for my $the ('great', 'lesser') {
+  for my $subj ($player, $clan) {
 
-    # GreatFoo needs the game to be ascended
-    next if $the eq 'great' && !$game->is_ascended();
+    # do nothing for non-clan games (ie. games by players who are not members
+    # of any clan)
 
-    # LesserFoo needs the game to achieve Sokoban and Mines' End luckstone
-    next if
-      $the eq 'lesser'
-      && !$game->has_achievement('sokoban', 'meluckstone');
+    next if !$subj;
 
-    for my $foo ('race', 'role') {
-      my $the_foo = "$the$foo";
-      for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
+    for my $the ('great', 'lesser') {
 
-        # player-specific tracking structure
+      # GreatFoo needs the game to be ascended
 
-        my $ptrk = $self->_player_track->{$player->name()};
+      next if $the eq 'great' && !$game->is_ascended();
 
-        # track role-race-align in MultiSet for greatrace; there's special case
-        # for Great Human that only requires _one_ Monk ascension, not all three
-        # available (Monks can be of any alignment)
+      # LesserFoo needs the game to achieve Sokoban and Mines' End luckstone
 
-        $ptrk->{$the_foo}{$rr}->track(
-          $rr => sprintf(
-            "%s-%s-%s",
-            $game->role(),
-            $game->race(),
-            (($game->role() eq 'Mon' && $foo eq 'race') ? '*' : $game->align0())
-          ),
-          $game
-        );
+      next if
+        $the eq 'lesser'
+        && !$game->has_achievement('sokoban', 'meluckstone');
 
+      for my $foo ('race', 'role') {
+        my $the_foo = "$the$foo";
+        for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
+
+          # get the tracking data
+
+          my $trk = $self->_track_data($subj);
+
+          # track role-race-align in MultiSet for greatrace; there's special
+          # case for Great Human that only requires _one_ Monk ascension, not
+          # all three available (Monks can be of any alignment)
+
+          $trk->{$the_foo}{$rr}->track(
+            $rr => sprintf(
+              "%s-%s-%s",
+              $game->role(),
+              $game->race(),
+              (
+                ($game->role() eq 'Mon' && $foo eq 'race')
+                ? '*' : $game->align0()
+              )
+            ),
+            $game
+          );
+
+        }
       }
     }
   }
@@ -182,46 +232,36 @@ sub finish
 
 sub export
 {
+  use Data::Dumper;
+
   my ($self) = @_;
   my $cfg = TNNT::Config->instance()->config();
 
-  #--- players
+  my %subjects;
 
-  my %players;
-
-  foreach my $player_name (keys %{$self->_player_track()}) {
-    my $ptrk = $self->_player_track()->{$player_name};
-    for my $the ('great', 'lesser' ) {
-      for my $foo ('race', 'role') {
-        my $the_foo = "$the$foo";
-        for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
-          if($ptrk->{$the_foo}{$rr}->track()) {
-            push(@{$players{"$the_foo:" . lc($rr)}}, $player_name);
+  foreach my $subj_type ('player', 'clan') {
+    my $trk = $self->_track($subj_type);
+    foreach my $subj_id (keys %$trk) {
+      my $strk = $trk->{$subj_id};
+      for my $the ('great', 'lesser' ) {
+        for my $foo ('race', 'role') {
+          my $the_foo = "$the$foo";
+          for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
+            if($strk->{$the_foo}{$rr}->track()) {
+              push(
+                @{$subjects{"${subj_type}s"}{"$the$foo:" . lc($rr)}},
+                $subj_id
+              );
+            }
           }
         }
       }
     }
   }
 
-  #--- clans
+  print Dumper(\%subjects), "\n";
 
-  my %clans;
-
-  foreach my $clan_idx (keys %{$self->_clan_track()}) {
-    my $ctrk = $self->_clan_track()->{$clan_idx};
-    for my $the ('great', 'lesser' ) {
-      for my $foo ('race', 'role') {
-        my $the_foo = "$the$foo";
-        for my $rr (keys %{$cfg->{'nethack'}{"great$foo"}}) {
-          if(exists $ctrk->{$the_foo}{$rr}) {
-            push(@{$clans{"$the_foo:" . lc($rr)}}, $clan_idx);
-          }
-        }
-      }
-    }
-  }
-
-  return (\%players, \%clans);
+  return ($subjects{'players'}, $subjects{'clans'});
 }
 
 
